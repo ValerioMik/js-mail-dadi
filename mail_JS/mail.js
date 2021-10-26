b51b0e9c098e0667689bd777b515021521efe144
ValerioMik <valerio.avallone@hotmail.com>
2021-10-26 15:22:59 +0200

    Macro struttura primo esercizio commentata

---
+++ mail_JS/mail.js
@@ -0,0 +1,15 @@
+/* Mail
+Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
+Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con le cose viste questa mattina. */
+
+/* creo una lista di mail per entrare */
+
+/* faccio si che l'utente inserisca la propria mail  */
+
+/* faciio in modo che il computer compari l'email dell'utente con la lista */
+
+/* se l'email e all'interno della lista */
+        /* stampo la scritta complimenti puoi entrare */
+        /* altrimenti stampo la scritta spiacenti non puo entrare la sua email non cmpare nella lista */
+        
+/* spero che tutto funzioni :-) */
