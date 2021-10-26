c8cf48e3963c6355c82531a72e1cb496b66f4741
ValerioMik <valerio.avallone@hotmail.com>
2021-10-26 17:59:27 +0200

    ho finito anche i dadi

--- dadi_js/dai.js
+++ dadi_js/dai.js
@@ -1,3 +1,21 @@
 /* Gioco dei dadi
 Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
 Stabilire il vincitore, in base a chi fa il punteggio più alto. */
+
+/* far generare un numero da 1 a 6 all'utente */
+    let numeroTente = Math.floor((Math.random() * 6) + 1)
+    console.log("questo e il tuo numero: "+numeroTente);
+/* far generare un numero da 1 a 6 al computer */
+    let numerocomputer = Math.floor((Math.random() * 6) + 1)
+    console.log("questo e il numero computer: "+numerocomputer);
+/* controllo del numero piu alto */
+    if(numeroTente > numerocomputer){
+        console.log("complimenti hai vinto tu");
+    }else if(numeroTente == numerocomputer){
+        console.log("avete pareggiato riprovate");
+    }else {
+        console.log("ha vinto il computer");
+    }
+    
+    
+/* stampo chi ha vinto */
--- mail_JS/mail.js
+++ mail_JS/mail.js
@@ -2,29 +2,26 @@
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può accedere (che hai fatto tu precedentemente), stampa un messaggio appropriato sull’esito del controllo.
 Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con le cose viste questa mattina. */
 
-/* creo una lista di mail per entrare */
 
-const emailAccesso = ["valerio@hotmail.com","marco123@live.it","andrea@hotmail.com","pier@gmail.com","massimo@tnt.it","alessandro@gmail.it" ];
-for (let i = 0; i < emailAccesso.length;i++ )
-/* console.log(emailAccesso[i]);
- */
 /* faccio si che l'utente inserisca la propria mail  */
-
 const emailUtent = prompt("prego inserisci la tua email");
-console.log(emailUtent);
+/* creo una lista di mail per entrare */
+const emailAccesso = ["valerio@hotmail.com","marco123@live.it","andrea@hotmail.com","pier@gmail.com","massimo@tnt.it","alessandro@gmail.it" ];
 
-/* faciio in modo che il computer compari l'email dell'utente con la lista */
+let emailinlista = false;
+for (let i = 0; i < emailAccesso.length; i++ ) {
+    /* faciio in modo che il computer compari l'email dell'utente con la lista */
+if (emailUtent === emailAccesso[i]) {
+     emailinlista = true;
+  }
+}
 /* se l'email e all'interno della lista */
-if(emailUtent == emailAccesso){
-    const acessCons = ("Accesso consentito")
-            /* stampo la scritta complimenti puoi entrare */
-    console.log(acessCons);
-}else (emailUtent != emailAccesso)
-        /* altrimenti stampo la scritta spiacenti non puo entrare la sua email non cmpare nella lista */
-    const acessNegato = ("Accesso negato non sei in lista")
-    console.log(acessNegato);
-
-
-        /* altrimenti stampo la scritta spiacenti non puo entrare la sua email non cmpare nella lista */
-
+        /* stampo la scritta complimenti puoi entrare */
+if (emailinlista === true) {
+    console.log('sei nella lista complimenti ora puoi entrare');
+}else {
+     /* altrimenti stampo la scritta spiacenti non puo entrare la sua email non cmpare nella lista */
+    console.log('spiacente email non trovata');
+}
+       
 /* spero che tutto funzioni :-) */
