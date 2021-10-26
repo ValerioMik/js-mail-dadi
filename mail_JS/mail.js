fdf92beba9b88ece81947a4453cb236836ff7a96
ValerioMik <valerio.avallone@hotmail.com>
2021-10-26 17:52:13 +0200

    dopo tante peripezie primo esercizio terminato

--- mail_JS/mail.js
+++ mail_JS/mail.js
@@ -4,12 +4,27 @@ Qui, come detto, NON possiamo usare metodi nuovi sugli array ma lo facciamo con 
 
 /* creo una lista di mail per entrare */
 
+const emailAccesso = ["valerio@hotmail.com","marco123@live.it","andrea@hotmail.com","pier@gmail.com","massimo@tnt.it","alessandro@gmail.it" ];
+for (let i = 0; i < emailAccesso.length;i++ )
+/* console.log(emailAccesso[i]);
+ */
 /* faccio si che l'utente inserisca la propria mail  */
 
-/* faciio in modo che il computer compari l'email dell'utente con la lista */
+const emailUtent = prompt("prego inserisci la tua email");
+console.log(emailUtent);
 
+/* faciio in modo che il computer compari l'email dell'utente con la lista */
 /* se l'email e all'interno della lista */
-        /* stampo la scritta complimenti puoi entrare */
+if(emailUtent == emailAccesso){
+    const acessCons = ("Accesso consentito")
+            /* stampo la scritta complimenti puoi entrare */
+    console.log(acessCons);
+}else (emailUtent != emailAccesso)
         /* altrimenti stampo la scritta spiacenti non puo entrare la sua email non cmpare nella lista */
-        
+    const acessNegato = ("Accesso negato non sei in lista")
+    console.log(acessNegato);
+
+
+        /* altrimenti stampo la scritta spiacenti non puo entrare la sua email non cmpare nella lista */
+
 /* spero che tutto funzioni :-) */
